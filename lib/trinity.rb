7af13d4b073df6158d300d7d95c908e59ec761e4
Arto Bendiken <arto@bendiken.net>
2010-01-26 15:02:20 +0100

    Added Trinity::Plugin.

--- lib/trinity.rb
+++ lib/trinity.rb
@@ -8,6 +8,7 @@ require 'trinity/version'
 module Trinity
   autoload :Application, 'trinity/core/application'
   autoload :Handler,     'trinity/core/handler'
+  autoload :Plugin,      'trinity/core/plugin'
   autoload :Renderer,    'trinity/core/renderer'
   autoload :Resource,    'trinity/core/resource'
   autoload :Server,      'trinity/core/server'
---
+++ lib/trinity/core/plugin.rb
@@ -0,0 +1,6 @@
+module Trinity
+  ##
+  class Plugin
+    # TODO
+  end
+end
