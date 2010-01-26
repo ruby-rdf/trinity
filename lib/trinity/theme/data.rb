0b2e786ed8d2d24a361fb9f1252e61b136db6001
Arto Bendiken <arto@bendiken.net>
2010-01-27 00:01:48 +0100

    Fleshed out Trinity::Theme::Data.

--- lib/trinity/theme/data.rb
+++ lib/trinity/theme/data.rb
@@ -7,94 +7,98 @@ class Trinity::Theme
     URI = RDF::URI.new('http://gemcutter.org/gems/trinity-theme') 
     NS  = RDF::Vocabulary.new("#{URI}#")
 
+    ##
+    # @param  [Application] application
+    # @return [void]
     def self.initialize!(application)
-      application.map('/theme.css', CSS)
-      application.map('/theme.js',  JS)
+      application.map('/theme/data.css', CSS)
+      application.map('/theme/data.js',  JS)
+    end
+
+    ##
+    # @param  [Renderer] renderer
+    # @return [void]
+    def self.render_html_head(renderer)
+      renderer.html do
+        link   :type => 'text/css', :rel => :stylesheet, :media => 'screen', :href => '/theme/data.css'
+        script :type => 'text/javascript', :language => :javascript, :src => '/theme/data.js'
+      end
+    end
+
+    ##
+    # @param  [Renderer] renderer
+    # @return [void]
+    def self.render_html_body(renderer)
+      theme = self
+      renderer.html do
+        table(:id => :statements) do
+          thead do
+            # TODO
+          end
+          tbody do
+            renderer.data.each do |statement|
+              tr(:class => statement) do
+                td(:class => :predicate) do
+                  text theme.render_predicate(statement)
+                end
+                td(:class => :object) do
+                  text theme.render_object(statement)
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
+    ##
+    # @param  [RDF::Statement] statement
+    # @return [String]
+    def self.render_predicate(statement)
+      escape_string(render_value(statement.predicate))
+    end
+
+    ##
+    # @param  [RDF::Statement] statement
+    # @return [String]
+    def self.render_object(statement)
+      escape_string(render_value(statement.object))
+    end
+
+    ##
+    # @param  [String] string
+    # @return [String]
+    def self.escape_string(string)
+      ERB::Util.html_escape(string)
+    end
+
+    ##
+    # @param  [RDF::Value] value
+    # @return [String]
+    def self.render_value(value)
+      value.inspect # TODO
     end
 
     ##
     class CSS < Trinity::Handler
       def call(env)
-        [200, {}, ['/* TODO */']]
+        [200, {'Content-Type' => 'text/css'}, [content]]
+      end
+
+      def content
+        '/* TODO */'
       end
     end
 
     ##
     class JS < Trinity::Handler
       def call(env)
-        [200, {}, ['/* TODO */']]
+        [200, {'Content-Type' => 'text/javascript'}, [content]]
       end
-    end
 
-    ##
-    # @param  [Builder]
-    # @return [void]
-    def self.render_html_head(renderer)
-      renderer.html do
-        link   :type => 'text/css', :rel => :stylesheet, :media => 'screen', :href => '/theme.css'
-        script :type => 'text/javascript', :language => :javascript, :src => '/theme.js'
+      def content
+        '/* TODO */'
       end
-    end
-
-    ##
-    # @param  [Builder]
-    # @return [void]
-    def self.render_html_body(renderer)
-      self.render_html_block(renderer, :header)
-      self.render_html_block(renderer, :title)
-      self.render_html_block(renderer, :document)
-      self.render_html_block(renderer, :footer)
-    end
-
-    ##
-    # @param  [Builder]
-    # @param  [Symbol, String, #to_s]
-    # @return [void]
-    def self.render_html_block(renderer, id)
-      this = self
-      renderer.html do
-        div(:id => id) { this.send("render_#{id}_block", renderer) }
-      end
-    end
-
-    ##
-    # @param  [Builder]
-    # @return [void]
-    def self.render_header_block(renderer)
-      # TODO
-    end
-
-    ##
-    # @param  [Builder]
-    # @return [void]
-    def self.render_title_block(renderer)
-      widget(renderer, ::RDF::DC.title, ::RDF::RDFS.label) rescue nil
-    end
-
-    ##
-    # @param  [Builder]
-    # @return [void]
-    def self.render_document_block(renderer)
-      widget(renderer, ::RDF::DC.description, ::RDF::RDFS.comment) rescue nil
-    end
-
-    ##
-    # @param  [Builder]
-    # @return [void]
-    def self.render_footer_block(renderer)
-      # TODO
-    end
-
-    ##
-    # @param  [RDF::URI] predicate
-    # @return [Widget]
-    def self.widget(renderer, *predicates)
-      predicates.each do |predicate|
-        if object = renderer.data.query([renderer.subject, predicate]).first.object rescue nil
-          widget = Trinity::Widget.for(predicate).new(object, :predicate => predicate)
-          return widget.content(renderer.builder)
-        end
-     end
     end
   end
 end
