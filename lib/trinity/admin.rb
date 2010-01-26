228cabee1b27473523b70b456788ef975f85bc33
Arto Bendiken <arto@bendiken.net>
2010-01-26 21:26:27 +0100

    Added Trinity::Admin::{JS, CSS} stubs.

--- lib/trinity/admin.rb
+++ lib/trinity/admin.rb
@@ -6,7 +6,22 @@ module Trinity
     NS  = RDF::Vocabulary.new("#{URI}#")
 
     def self.initialize!(application)
-      # TODO
+      application.map('/trinity.css') { run CSS.new(application) }
+      application.map('/trinity.js')  { run JS.new(application) }
+    end
+
+    ##
+    class JS < Trinity::Handler
+      def call(env)
+        [200, {}, ['/* TODO */']]
+      end
+    end
+
+    ##
+    class CSS < Trinity::Handler
+      def call(env)
+        [200, {}, ['/* TODO */']]
+      end
     end
   end
 end
--- lib/trinity/core/application.rb
+++ lib/trinity/core/application.rb
@@ -4,10 +4,7 @@ module Trinity
     def initialize(repository, options = {}, &block)
       this = self
       @repository  = repository
-      @application = Rack::Builder.app do
-        map('/trinity.js') do
-          run Trinity::Handler::Admin.new(this, repository)
-        end
+      @application = Rack::Builder.new do
         map(options[:path] || '/') do
           use Trinity::Handler::Initializer, repository, options
           use Trinity::Handler::Acceptor,    repository
