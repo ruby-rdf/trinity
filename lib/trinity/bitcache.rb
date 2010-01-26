684a3e97bf23e23400acb1d1fa568238eead7d1e
Arto Bendiken <arto@bendiken.net>
2010-01-26 18:11:40 +0100

    Defined RDF namespaces for the plugins.

--- lib/trinity/bitcache.rb
+++ lib/trinity/bitcache.rb
@@ -4,6 +4,8 @@ module Trinity
   #
   # @see http://bitcache.org/
   class Bitcache < Trinity::Plugin
+    NS = RDF::Vocabulary.new("http://gemcutter.org/gems/trinity-bitcache#")
+
     def self.initialize!(application)
       require 'bitcache' unless defined?(::Bitcache)
     end
